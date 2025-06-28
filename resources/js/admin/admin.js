e6399bcec0052d6fe1f491c21a33cf0e00977c30
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2025-06-28 18:25:45 +0300

    axios api

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
 import 'bootstrap';
+import { api } from './api';
 
 document.querySelectorAll('.category-name-js').forEach(element => {
     element.addEventListener('blur', async () => {
         const categoryId = element.closest('tr').dataset.id;
         const categoryName = element.textContent;
         
-       const response = await axios.patch(`/admin/categories/${categoryId}`, {
+        const response = await api.patch(`categories/${categoryId}`, {
             name: categoryName
        });
     
